5cb50d9b2db4b2fdc1117e527689eefe3c51347f
yanayakhontova <99177495+yanayakhontova@users.noreply.github.com>
2022-03-14 00:03:30 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,24 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+int c = 0, s = 0, d = size - 1;
+while (s < d) {
+int m = (s + d) / 2;
+if (arr[m] == value) {
+c++;
+int t = m;
+while (arr[--m] == value) {
+c++;
+}
+while (arr[++t] == value) {
+c++;
+}
+return c;
+} else if (arr[m] > value) {
+d = m;
+} else {
+s = m + 1;
+}
+}
+return 0; // если ничего не найдено
 }
